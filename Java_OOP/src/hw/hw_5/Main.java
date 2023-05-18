b2fb3335f8329967a54ec472207131fda582a55d
«IrenSk» <skiv.study@gmail.com>
2023-05-18 19:04:16 +0300

    Add HW_5

---
+++ Java_OOP/src/hw/hw_5/Main.java
@@ -0,0 +1,13 @@
+/**Бронирование столика в ресторане */
+package hw.hw_5;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // задача 1 ДЗ_5
+    }
+}
